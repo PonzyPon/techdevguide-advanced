a4ff7f83139099f50fc9090408db8aa5615737eb
PonzyPon <11908295+PonzyPon@users.noreply.github.com>
2020-06-22 23:22:08 +0900

    Implement trap method

--- rainwater/main.go
+++ rainwater/main.go
@@ -4,22 +4,30 @@ import (
 	"math"
 )
 
-func compute(arg []int) int {
-	// その数の左右を見て、その数より大きい数が左右ともにあれば水はたまる
-	// たまる量はその少ない方
+func trap(height []int) int {
 	var totalRain int
-	for val, i := range arg {
-		// 左側の最大値取得
-		// 右側の最大値取得
-		// 小さい方とvalの差分が水の量
+	length := len(height)
+	leftMaxArray, rightMaxArray := getMaxArrays(height)
+	for i, val := range height {
+		min := int(math.Min(float64(leftMaxArray[i]), float64(rightMaxArray[length-i-1])))
+		if min > val {
+			totalRain += min - val
+		}
 	}
-	return 0
+	return totalRain
 }
 
-func getLeftMax(arr []int, index int) int {
-	var max float64
-	for i := 0; i < index; i++ {
-		max = math.Max(float64(max), float64(arr[i]))
+func getMaxArrays(arr []int) ([]int, []int) {
+	leftMax := 0.0
+	leftMaxArray := []int{}
+	rightMax := 0.0
+	rightMaxArray := []int{}
+	length := len(arr)
+	for i := 0; i < len(arr); i++ {
+		leftMax = math.Max(leftMax, float64(arr[i]))
+		leftMaxArray = append(leftMaxArray, int(leftMax))
+		rightMax = math.Max(rightMax, float64(arr[length-1-i]))
+		rightMaxArray = append(rightMaxArray, int(rightMax))
 	}
-	return int(max)
+	return leftMaxArray, rightMaxArray
 }
--- rainwater/main_test.go
+++ rainwater/main_test.go
@@ -2,14 +2,14 @@ package rainwater
 
 import "testing"
 
-func TestCompute(t *testing.T) {
+func TestTrap(t *testing.T) {
 	// arrange
 	argument := []int{1, 3, 2, 4, 1, 3, 1, 4, 5, 2, 2, 1, 4, 2, 2}
 	want := 15
 	// act
-	got := compute(argument)
+	got := trap(argument)
 	// assert
 	if got != want {
-		t.Errorf("compute(%v) = %v, want %v", argument, got, want)
+		t.Errorf("trap(%v) = %v, want %v", argument, got, want)
 	}
 }
